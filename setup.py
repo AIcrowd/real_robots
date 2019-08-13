bc8dd91b38038a43059b78cf0be958a4dc1b2438
SP Mohanty <spmohanty91@gmail.com>
2019-08-13 18:32:17 +0200

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/spMohanty/real_robots',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
