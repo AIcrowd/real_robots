cd18eb7ca92c173b12fe0372e33548ac9156940a
Emilio Cartoni <emilio.cartoni@yahoo.it>
2019-08-17 22:22:43 +0200

    generate goals now saves in compressed format

--- real_robots/generate_goals.py
+++ real_robots/generate_goals.py
@@ -437,7 +437,7 @@ def main(seed=None, n1=0, n2=0, n3=0, n4=0, n5=0, n6=0, n7=0):
     for _ in range(n7):
         allgoals += [generateGoal3D(env)]
 
-    np.save('allgoals{}-{}-{}-{}-{}-{}-{}-{}.npy'
+    np.savez('allgoals{}-{}-{}-{}-{}-{}-{}-{}.npy'
             .format(seed, n1, n2, n3, n4, n5, n6, n7), allgoals)
 
     # checkRepeatability(env, allgoals)
