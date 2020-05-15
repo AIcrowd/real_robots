770b17964bb16195cc033aaef68e64e02a1210aa
Emilio Cartoni <emilio.cartoni@yahoo.it>
2020-05-15 15:29:34 +0200

    Improve object positions and action spaces

--- real_robots/envs/env.py
+++ real_robots/envs/env.py
@@ -41,13 +41,13 @@ class REALRobotEnv(MJCFBaseBulletEnv):
         self.joints_space = self.robot.action_space
 
         self.cartesian_space = spaces.Box(
-                           low=np.array([-0.5, -0.5, 0.35, -1, -1, -1, -1]),
-                           high=np.array([-0.1, 0.5, 0.60,  1,  1,  1,  1]),
+                           low=np.array([-0.25, -0.5, 0.40, -1, -1, -1, -1]),
+                           high=np.array([0.25, 0.5, 0.60,  1,  1,  1,  1]),
                            dtype=float)
 
         self.macro_space = spaces.Box(
-                                  low=np.array([[-0.5, -0.5], [-0.5, -0.5]]),
-                                  high=np.array([[-0.1, 0.5], [-0.1, 0.5]]),
+                                  low=np.array([[-0.25, -0.5], [-0.25, -0.5]]),
+                                  high=np.array([[0.05, 0.5], [0.05, 0.5]]),
                                   dtype=float)
 
         self.gripper_space = spaces.Box(low=0,
